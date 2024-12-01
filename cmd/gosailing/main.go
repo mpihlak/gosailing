7b917b8e7a9f6e3acd24432fc25a4093f3f28ae4
Martin Pihlak <martin.pihlak@starship.co>
2024-12-01 11:07:11 +0200

    Split sailrace to separate package

--- cmd/gosailing/main.go
+++ cmd/gosailing/main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"math"
-	"time"
-
 	"github.com/gopxl/pixel/v2"
 	"github.com/gopxl/pixel/v2/backends/opengl"
-	"github.com/gopxl/pixel/v2/ext/imdraw"
-	"github.com/gopxl/pixel/v2/ext/text"
 	"golang.org/x/image/colornames"
-	"golang.org/x/image/font/basicfont"
 
 	"github.io/mpihlak/gosailing"
 )
@@ -19,59 +12,12 @@ const (
 	maxWidth  = 1024.0
 	maxHeight = 768.0
 
-	windDirection = 10.0
+	windDirection = 0.0 // 0 is north, 90 is east and so on
 	markLocationX = maxWidth / 2
 	markLocationY = maxHeight - 50
+	boatLocationX = maxWidth / 2
+	boatLocationY = 25
 )
-
-type SailRace struct {
-	raceCourse *gosailing.RaceCourse
-	boat       *gosailing.Boat
-	wind       *gosailing.WindShifter
-	track      *gosailing.TrackPlotter
-	lastTack   time.Time
-	steps      int
-	paused     bool
-	delayMs    int
-	started    bool
-	finished   bool
-	race       *imdraw.IMDraw
-}
-
-func NewSailRace() *SailRace {
-	return &SailRace{
-		raceCourse: gosailing.NewRaceCourse(markLocationX, markLocationY, windDirection),
-		boat:       gosailing.NewBoat(maxWidth/2, 0, windDirection),
-		wind:       gosailing.NewWindShifter(windDirection, 10.0, 10),
-		track:      gosailing.NewTrackPlotter(),
-		delayMs:    50,
-	}
-}
-
-func (sr *SailRace) StartRace() {
-	sr.started = true
-}
-
-func (sr *SailRace) Draw(win *opengl.Window) {
-	sr.boat.Drawable().Draw(win)
-	sr.raceCourse.Drawable().Draw(win)
-	sr.track.Drawable().Draw(win)
-
-	basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
-
-	if !sr.started {
-		basicTxt := text.New(pixel.V(50, 100), basicAtlas)
-		basicTxt.Color = colornames.Black
-		fmt.Fprintln(basicTxt, "Press SPACE to start, 'q' to quit")
-		basicTxt.Draw(win, pixel.IM.Scaled(basicTxt.Orig, 2))
-	}
-	if sr.paused {
-		basicTxt := text.New(pixel.V(50, 100), basicAtlas)
-		basicTxt.Color = colornames.Black
-		fmt.Fprintln(basicTxt, "Paused, press SPACE to unpause")
-		basicTxt.Draw(win, pixel.IM.Scaled(basicTxt.Orig, 2))
-	}
-}
 
 func run() {
 	cfg := opengl.WindowConfig{
@@ -84,69 +30,41 @@ func run() {
 		panic(err)
 	}
 
-	sailRace := NewSailRace()
+	newSailRace := func() *gosailing.SailRace {
+		return gosailing.NewSailRace(
+			markLocationX, markLocationY,
+			boatLocationX, boatLocationY,
+			windDirection,
+		)
+	}
+
+	sailRace := newSailRace()
+
 	for !win.Closed() {
 		if win.Pressed(pixel.KeyQ) || win.Pressed(pixel.KeyEscape) {
 			break
 		}
 		if win.Pressed(pixel.Key1) {
-			sailRace.delayMs = max(0, sailRace.delayMs-10)
+			sailRace.IncreaseSpeed()
 		}
 		if win.Pressed(pixel.Key2) {
-			sailRace.delayMs += 10
+			sailRace.DecreaseSpeed()
 		}
-
 		if win.Pressed(pixel.KeySpace) {
-			if sailRace.started {
-				sailRace.paused = !sailRace.paused
-			} else {
-				sailRace.StartRace()
-			}
-			time.Sleep(100 * time.Millisecond)
+			sailRace.TogglePause()
 		}
-
-		if sailRace.started {
-			if win.Pressed(pixel.KeyT) {
-				if time.Since(sailRace.lastTack) > 500*time.Millisecond {
-					sailRace.boat.Tack()
-					sailRace.lastTack = time.Now()
-				}
-			}
-			if win.Pressed(pixel.KeyR) {
-				sailRace = NewSailRace()
-			}
+		if win.Pressed(pixel.KeyT) {
+			sailRace.TackBoat()
+		}
+		if win.Pressed(pixel.KeyR) {
+			sailRace = newSailRace()
 		}
 
 		win.Clear(colornames.Lightblue)
-
-		currentBoatX, currentBoatY := sailRace.boat.GetXY()
-		if sailRace.started && !sailRace.paused && currentBoatY <= markLocationY {
-			sailRace.track.PlotLocation(sailRace.boat.GetXY())
-			sailRace.boat.Advance()
-			windDirection := sailRace.wind.GetWindDirection()
-			sailRace.boat.SetWindDirection(windDirection)
-			sailRace.raceCourse.SetWindDirection(windDirection)
-			sailRace.steps++
-		} else if currentBoatY > markLocationY {
-			sailRace.finished = true
-			basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
-			basicTxt := text.New(pixel.V(50, maxHeight-50), basicAtlas)
-			basicTxt.Color = colornames.Black
-
-			if currentBoatX < sailRace.raceCourse.MarkX {
-				fmt.Fprintln(basicTxt, "Wrong side of the mark!")
-			}
-
-			extraDistance := math.Hypot(currentBoatX-markLocationX, currentBoatY-markLocationY)
-			fmt.Fprintf(basicTxt, "Sailed distance: %.2f pixels (+extra %.2f to mark)\n", sailRace.boat.GetSailedDistance(), extraDistance)
-			fmt.Fprintf(basicTxt, "Sailed time: %d units\n", sailRace.steps)
-
-			basicTxt.Draw(win, pixel.IM.Scaled(basicTxt.Orig, 2))
-		}
-
-		sailRace.Draw(win)
+		sailRace.Update(win)
 		win.Update()
-		time.Sleep(time.Duration(sailRace.delayMs) * time.Millisecond)
+
+		sailRace.Throttle()
 	}
 }
 
---
+++ sailrace.go
@@ -0,0 +1,151 @@
+package gosailing
+
+import (
+	"fmt"
+	"math"
+	"time"
+
+	"github.com/gopxl/pixel/v2"
+	"github.com/gopxl/pixel/v2/backends/opengl"
+	"github.com/gopxl/pixel/v2/ext/imdraw"
+	"github.com/gopxl/pixel/v2/ext/text"
+	"golang.org/x/image/colornames"
+	"golang.org/x/image/font/basicfont"
+)
+
+type SailRace struct {
+	raceCourse *RaceCourse
+	boat       *Boat
+	wind       *WindShifter
+	track      *TrackPlotter
+	lastTack   time.Time
+	delayMs    int
+	paused     bool
+	started    bool
+	finished   bool
+	race       *imdraw.IMDraw
+}
+
+func NewSailRace(markLocationX, markLocationY, boatLocationX, boatLocationY, windDirection float64) *SailRace {
+	return &SailRace{
+		raceCourse: NewRaceCourse(markLocationX, markLocationY, windDirection),
+		boat:       NewBoat(boatLocationX, boatLocationY, windDirection),
+		wind:       NewWindShifter(windDirection, 10.0, 10),
+		track:      NewTrackPlotter(),
+		delayMs:    50,
+	}
+}
+
+func (sr *SailRace) StartRace() {
+	sr.started = true
+}
+
+func (sr *SailRace) IncreaseSpeed() {
+	sr.delayMs = max(0, sr.delayMs-10)
+}
+
+func (sr *SailRace) DecreaseSpeed() {
+	sr.delayMs += 10
+}
+
+func (sr *SailRace) TogglePause() {
+	if sr.started {
+		sr.paused = !sr.paused
+
+		// Small delay to eat the keypress
+		time.Sleep(200 * time.Millisecond)
+	} else {
+		sr.StartRace()
+	}
+}
+
+func (sr *SailRace) TackBoat() {
+	if sr.started {
+		if time.Since(sr.lastTack) > 500*time.Millisecond {
+			sr.boat.Tack()
+			sr.lastTack = time.Now()
+		}
+	}
+}
+
+func (sr *SailRace) Throttle() {
+	time.Sleep(time.Duration(sr.delayMs) * time.Millisecond)
+}
+
+func (sr *SailRace) Update(win *opengl.Window) {
+	windowBounds := win.Bounds()
+	topLeftY := windowBounds.H()
+
+	currentBoatX, currentBoatY := sr.boat.GetXY()
+
+	// TODO: refactor the finishing condition so that it also works on non-vertical courses
+
+	if sr.started && !sr.paused && currentBoatY <= sr.raceCourse.MarkY {
+		sr.track.PlotLocation(sr.boat.GetXY())
+		sr.boat.Advance()
+		windDirection := sr.wind.GetWindDirection()
+		sr.boat.SetWindDirection(windDirection)
+		sr.raceCourse.SetWindDirection(windDirection)
+	} else if currentBoatY > sr.raceCourse.MarkY {
+		sr.finished = true
+	}
+
+	basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
+
+	if sr.started {
+		basicTxt := text.New(pixel.V(10, topLeftY-25), basicAtlas)
+		basicTxt.Color = colornames.Black
+
+		distanceToMark := math.Hypot(currentBoatX-sr.raceCourse.MarkX, currentBoatY-sr.raceCourse.MarkY)
+		fmt.Fprintf(basicTxt, "Sailed distance:  %.2f\n", sr.boat.GetSailedDistance())
+		fmt.Fprintf(basicTxt, "Distance to mark: %.2f\n", distanceToMark)
+		if sr.finished {
+			basicTxt.Color = colornames.Darkblue
+			fmt.Fprintln(basicTxt, "FINISHED!")
+
+			if currentBoatX < sr.raceCourse.MarkX {
+				basicTxt.Color = colornames.Red
+				fmt.Fprintln(basicTxt, "Wrong side of the mark!")
+			}
+		}
+
+		basicTxt.Draw(win, pixel.IM.Scaled(basicTxt.Orig, 2))
+	}
+
+	sr.boat.Drawable().Draw(win)
+	sr.raceCourse.Drawable().Draw(win)
+	sr.track.Drawable().Draw(win)
+
+	if !sr.started {
+		textX := windowBounds.Center().X - 150
+		textY := windowBounds.Center().Y + 100
+
+		lines := []string{
+			"Press SPACE to start or pause",
+			"'q' quits",
+			"'t' tacks'",
+			"'r' restarts'",
+			"'1' increases speed'",
+			"'2' decreases speed'",
+		}
+
+		basicTxt := text.New(pixel.V(textX, textY), basicAtlas)
+		basicTxt.Color = colornames.Black
+		for _, line := range lines {
+			fmt.Fprintln(basicTxt, line)
+		}
+		basicTxt.Draw(win, pixel.IM.Scaled(basicTxt.Orig, 2))
+	}
+
+	if sr.paused {
+		textX := windowBounds.Center().X
+		textY := windowBounds.Center().Y
+		pauseBanner := "Paused, press SPACE to unpause"
+
+		basicTxt := text.New(pixel.V(textX, textY), basicAtlas)
+		basicTxt.Dot.X -= basicTxt.BoundsOf(pauseBanner).W() / 2
+		basicTxt.Color = colornames.Black
+		fmt.Fprintln(basicTxt, pauseBanner)
+		basicTxt.Draw(win, pixel.IM.Scaled(basicTxt.Orig, 2))
+	}
+}
