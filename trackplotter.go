1d5932301df0c2e3bade3bb2da48089353935c1a
Martin Pihlak <martin.pihlak@starship.co>
2024-12-15 11:56:55 +0200

    Variable naming

--- trackplotter.go
+++ trackplotter.go
@@ -9,7 +9,7 @@ import (
 )
 
 type TrackPlotter struct {
-	track    *imdraw.IMDraw
+	canvas   *imdraw.IMDraw
 	plottedX float64
 	plottedY float64
 }
@@ -18,25 +18,25 @@ func NewTrackPlotter(x, y float64) *TrackPlotter {
 	return &TrackPlotter{
 		plottedX: x,
 		plottedY: y,
-		track:    imdraw.New(nil),
+		canvas:   imdraw.New(nil),
 	}
 }
 
 func (tp *TrackPlotter) PlotLocation(x, y float64) {
 	distance := math.Hypot(tp.plottedX-x, tp.plottedY-y)
 	if distance > 5 {
-		tp.track.Color = colornames.Blueviolet
-		tp.track.Push(pixel.V(tp.plottedX, tp.plottedY))
-		tp.track.Circle(1, 1)
+		tp.canvas.Color = colornames.Blueviolet
+		tp.canvas.Push(pixel.V(tp.plottedX, tp.plottedY))
+		tp.canvas.Circle(1, 1)
 		tp.plottedX = x
 		tp.plottedY = y
 	}
 }
 
 func (tp *TrackPlotter) Clear() {
-	tp.track.Clear()
+	tp.canvas.Clear()
 }
 
 func (tp *TrackPlotter) Drawable() *imdraw.IMDraw {
-	return tp.track
+	return tp.canvas
 }
